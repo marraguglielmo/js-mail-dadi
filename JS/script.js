3318dcb6f311ca068e4c38506030e018e920f691
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-23 16:17:28 +0100

    [fix] variabile message

--- JS/script.js
+++ JS/script.js
@@ -2,15 +2,16 @@
 const emailList = ["gugmarra@gmail.com", "pincopallino@gmail.com", "marcoRossi85@gmail.com"];
 
 const emailUser = prompt("Inserisci email");
+let message;
 
 for(let i = 0; i < emailList.length; i++){
-    let message;
 
     if(emailUser === emailList[i]){
         message = "L'email è corretta. BENVENUTO" ;
     }else{
         message = "!L'email inserita non è corretta!"
     }
-
-    console.log(message);
+    
 }
+
+console.log(message);
