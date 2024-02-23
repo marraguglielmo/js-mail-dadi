1b9aa553c994c99062ec0b3ded45d159c77adbe2
Guglielmo Marra <156308467+marraguglielmo@users.noreply.github.com>
2024-02-23 18:01:02 +0100

    [add] gioco dei dadi

--- JS/script.js
+++ JS/script.js
@@ -1,40 +1,42 @@
 // MAIL
-const emailList = ["gugmarra@gmail.com", "pincopallino@gmail.com", "marcoRossi85@gmail.com", "gianfrancobiscotto@gmail.com", "luigibello@gmail.com"];
+// const emailList = ["gugmarra@gmail.com", "pincopallino@gmail.com", "marcoRossi85@gmail.com", "gianfrancobiscotto@gmail.com", "luigibello@gmail.com"];
 
-const emailUser = prompt("Inserisci email");
-let mailFound = false;
+// const emailUser = prompt("Inserisci email");
+// let mailFound = false;
 
-for(let i = 0; i < emailList.length; i++){
+// for(let i = 0; i < emailList.length; i++){
 
-    if(emailUser === emailList[i]){
-        mailFound = true;
-        // stoppo il ciclo
-        i = emailList.length;
-    }
-}
-// in base all'esito del ciclo stampo il risultato
-if(mailFound){
-    console.log("tutto ok");
-}else{
-    console.log("sbagliata");
-}
+//     if(emailUser === emailList[i]){
+//         mailFound = true;
+//         // stoppo il ciclo
+//         i = emailList.length;
+//     }
+// }
+// // in base all'esito del ciclo stampo il risultato
+// if(mailFound){
+//     console.log("L'email inserita è valida");
+// }else{
+//     console.log("!L'email inserita non è valida!");
+// }
 
 
 
 // GIOCO DEI DADI
 
-// const userList = [1, 2, 3, 4, 5, 6];
-// const pcList = [1, 2, 3, 4, 5, 6];
+const userList = [1, 2, 3, 4, 5, 6];
+const pcList = [1, 2, 3, 4, 5, 6];
 
-// const numuser = Math.ceil(Math.random() * userList.length);
-// console.log(numuser);
+const numuser = Math.ceil(Math.random() * userList.length);
 
-// const numPc = Math.ceil(Math.random() * pcList.length);
-// console.log(numPc);
+const numPc = Math.ceil(Math.random() * pcList.length);
 
-// if(numuser > numPc){
-//     console.log("Il vincitore è " + userList);
-// }else if(numuser < numPc){
-//     console.log("Il vincitore è " + userList);
-    
-// }
+console.log("Il giocatore ha pescato " + numuser);
+console.log("Il PC ha pescato " + numPc);
+
+if(numuser > numPc){
+    console.log("Il vincitore è il giocatore");
+}else if(numuser < numPc){
+    console.log("Il vincitore è il PC");
+}else{
+    console.log("I numeri pescati sono uguali: PARITÀ");
+}
